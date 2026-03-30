15e9d02e53cad386d9ac50cd737911de6e1e2ef2
unknown <shaivamuthaiya@gmail.com>
2026-03-30 14:23:31 +0200

    message class

--- src/Message.java
+++ src/Message.java
@@ -1,32 +1,42 @@
 import java.util.ArrayList;
 import java.awt.*;
+import java.io.Serializable;
 
 
-public class Message {
+public class Message implements Serializable {
+
+
     // A completer en fonction des besoins
     private String senderId;
     private String receiverId;
     private Point senderCoordinate;
     private String content;
-    // private String currentAntenna;
-    // private String previousAntenna;
-    private ArrayList<String> route = new ArrayList<>();
+    public String originalAntennaId;
+    public String previousAntennaId;
+
+    // private ArrayList<String> route = new ArrayList<>();
 
 
-    public Message(String senderId, String receiverId, String content, Point senderPositionPoint) {
+    public Message(String senderId, String receiverId, String content, Point senderPositionPoint, String originalAntennaId) {
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.content = content;
         this.senderCoordinate = senderPositionPoint;
+        this.originalAntennaId = originalAntennaId; // Store the original antenna ID in the message
+        this.previousAntennaId = originalAntennaId; // Initialize previousAntennaId to originalAntennaId
+
 
     }
 
+
+
+    // Getters
     public String getSenderId() { return senderId; }
     public String getReceiverId() { return receiverId; }
     public String getContent() { return content; }
+    public String getOriginalAntennaId() { return originalAntennaId; }
+    public String getPreviousAntennaId() { return previousAntennaId; }
+    public Point getSenderCoordinate() { return senderCoordinate; }
 
 
-    public void updateHeader(String sourceAntenna) {
-        this.route.add(sourceAntenna);
-    }
 }
